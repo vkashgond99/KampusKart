ad2431dfd02e9ec4c26379cd759df571a8e9cc72
VIKASH KUMAR GOND <167673289+vkashgond99@users.noreply.github.com>
2026-01-28 17:28:44 +0530

    Create Chat.js

---
+++ server/models/Chat.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const chatSchema = mongoose.Schema({
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User" 
+    }],
+    latestMessage: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Message" 
+    },
+}, { timestamps: true });
+
+const Chat = mongoose.model("Chat", chatSchema);
+export default Chat;
